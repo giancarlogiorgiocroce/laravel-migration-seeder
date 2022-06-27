9ae4a472339cee1f8ea9087a4bf99381707c3962
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-27 16:13:38 +0200

    Database popolato con faker

--- laravel-migration-seeder/database/seeds/TrainsTableSeeder.php
+++ laravel-migration-seeder/database/seeds/TrainsTableSeeder.php
@@ -12,8 +12,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 30; $i++) {
+            $new_train = new Train();
+            $new_train->company = $faker->word();
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_number = $faker->randomNumber(5, true);
+            $new_train->train_carriage_quantity = $faker->numberBetween(2, 20);
+            $new_train->is_on_time = $faker->boolean();
+            $new_train->is_cancelled = $faker->boolean();
+            $new_train->save();
+        }
     }
 }
