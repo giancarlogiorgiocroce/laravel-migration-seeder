751f5364629c3637f515ec62f11c141933250cbd
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-27 15:48:24 +0200

    Sostituito il faker e importato (insieme al model) nel seeder

---
+++ laravel-migration-seeder/database/seeds/TrainsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Train;
+use Faker\Generator as Faker;
+
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
